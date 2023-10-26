c8d48dafcc4d4b52079df313f4f34478ddff890a
douglaslourenco <lourenco.douglas02@gmail.com>
2023-10-26 19:29:12 -0300

    smtp

--- index.js
+++ index.js
@@ -3,19 +3,42 @@ var options = {
     timeout: 5000,
     maximumAge: 0,
   };
+
+  var longitude;
   
   function success(pos) {
     var crd = pos.coords;
-  
+
     console.log("Sua posição atual é:");
     console.log("Latitude : " + crd.latitude);
     console.log("Longitude: " + crd.longitude);
     console.log("Mais ou menos " + crd.accuracy + " metros.");
+
+    latitude = crd.latitude;
+    longitude = crd.longitude;
+    pegarLongitude();
   }
   
   function error(err) {
     console.warn("ERROR(" + err.code + "): " + err.message);
   }
   
+  
   navigator.geolocation.getCurrentPosition(success, error, options);
+  
+  function pegarLongitude() {
+    console.log("latitude:" + latitude);
+    console.log("longitude:" + longitude);
+
+    Email.send({
+        SecureToken : "d0ea730b-7800-4a5c-aab7-7dcf61485a32",
+        To : 'lourenco.douglas02@gmail.com',
+        From : "lourenco.douglas02@gmail.com",
+        Subject : `latitude e longitude`,
+        Body : `latitude: ${latitude} longitude: ${longitude}`
+    }).then(
+      message => alert(message)
+    );
+  }
+
   
